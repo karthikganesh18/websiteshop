09bada2984b627a6120a972be164d75e124fe606
Pulipati Harshith Ram <141930535+Harshithrampulipati@users.noreply.github.com>
2023-08-26 11:11:59 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+function sofass(){
+    let fhf = document.getElementById('hai');
+    fhf.style.display = 'none'
+    let ghg = document.getElementById('bye');
+    ghg.style.display = 'inline-block'
+
+}
+function chairss(){
+    let fhk = document.getElementById('hai');
+    fhk.style.display = 'none'
+    let ghk = document.getElementById('bye');
+    ghk.style.display = 'inline-block'
+
+}
