777b067fc5d37e072a0dd3600086f882aaf642f5
Pulipati Harshith Ram <141930535+Harshithrampulipati@users.noreply.github.com>
2023-08-26 11:49:52 +0530

    main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function sofass(){
     let fhf = document.getElementById('hai');
     fhf.style.display = 'none'
+    
     let ghg = document.getElementById('bye');
     ghg.style.display = 'inline-block'
 
@@ -8,7 +9,80 @@ function sofass(){
 function chairss(){
     let fhk = document.getElementById('hai');
     fhk.style.display = 'none'
-    let ghk = document.getElementById('bye');
+    let fhu = document.getElementById('bye2');
+    fhu.style.display = 'none'
+    let fhw = document.getElementById('bye3');
+    fhw.style.display = 'none'
+    let ghq = document.getElementById('bye1');
+    ghq.style.display = 'inline-block'
+
+}
+function tabless(){
+    let fhk = document.getElementById('hai');
+    fhk.style.display = 'none'
+    let ghk = document.getElementById('bye2');
+    ghk.style.display = 'inline-block'
+    let fhf = document.getElementById('bye');
+    fhf.style.display = 'none'
+
+}
+
+function recliners(){
+    let fhk = document.getElementById('hai');
+    fhk.style.display = 'none'
+    let ghk = document.getElementById('bye3');
     ghk.style.display = 'inline-block'
 
 }
+
+function remove1(){
+    let fhf = document.getElementById('hai');
+    fhf.style.display = 'inline-block'
+    let ghg = document.getElementById('bye');
+    ghg.style.display = 'none'
+    let ghg3 = document.getElementById('bye3');
+    ghg3.style.display = 'none'
+   
+}
+
+function remove2(){
+    let fhf = document.getElementById('hai');
+    fhf.style.display = 'inline-block'
+    let ghg = document.getElementById('bye');
+    ghg.style.display = 'none'
+    let ghg1 = document.getElementById('bye1');
+    ghg1.style.display = 'none'
+    let ghg2 = document.getElementById('bye2');
+    ghg2.style.display = 'none'
+    let ghg3 = document.getElementById('bye3');
+    ghg3.style.display = 'none'
+}
+
+function remove3(){
+    let fhf = document.getElementById('hai');
+    fhf.style.display = 'inline-block'
+    let ghg = document.getElementById('bye');
+    ghg.style.display = 'none'
+    let ghg1 = document.getElementById('bye1');
+    ghg1.style.display = 'none'
+    let ghg2 = document.getElementById('bye2');
+    ghg2.style.display = 'none'
+    let ghg3 = document.getElementById('bye3');
+    ghg3.style.display = 'none'
+}
+   
+
+
+function remove4(){
+    let fhf = document.getElementById('hai');
+    fhf.style.display = 'inline-block'
+    let ghg = document.getElementById('bye');
+    ghg.style.display = 'none'
+    let ghg1 = document.getElementById('bye1');
+    ghg1.style.display = 'none'
+    let ghg2 = document.getElementById('bye2');
+    ghg2.style.display = 'none'
+    let ghg3 = document.getElementById('bye3');
+    ghg3.style.display = 'none'
+}
+   
